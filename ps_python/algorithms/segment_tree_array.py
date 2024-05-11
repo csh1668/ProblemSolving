6f978ed7912cc2bf12ad869868da1926ac213b6d
SeoHyeon <tjgus1668@naver.com>
2024-05-11 22:37:17 +0900

    edited segtree

--- ps_python/algorithms/segment_tree_array.py
+++ ps_python/algorithms/segment_tree_array.py
@@ -2,10 +2,12 @@ import math
 
 
 class ArraySegmentTree:
-    def __init__(self, init_arr):
+    def __init__(self, init_arr, init_val, combine_func):
         n = len(init_arr)
         self.init_arr = init_arr
-        self.tree = [0] * pow(2, math.ceil(math.log(n, 2) + 1))
+        self.init_val = init_val
+        self.combine_func = combine_func
+        self.tree = [init_val] * pow(2, math.ceil(math.log(n, 2) + 1))
         self.location = [0] * n
         self.make_segment_tree(0, n - 1, 1)
 
@@ -18,23 +20,23 @@ class ArraySegmentTree:
             return self.tree[i]
         mid = (left + right) // 2
         # segment에 대한 연산
-        self.tree[i] = self.make_segment_tree(left, mid, i * 2) + self.make_segment_tree(mid + 1, right, i * 2 + 1)
+        self.tree[i] = self.combine_func(self.make_segment_tree(left, mid, i * 2), self.make_segment_tree(mid + 1, right, i * 2 + 1))
         return self.tree[i]
 
     def search(self, start, end, left, right, i=1):
         # 범위를 벗어나면 기본값 리턴
         if end < left or start > right:
-            return 0
+            return self.init_val
         # 찾는 범위 왼쪽이 구간 왼쪽보다 작거나 같고,
         # 찾는 범위 오른쪽이 구간 오른쪽보다 크거나 같으면 구간값 리턴
         if left <= start and end <= right:
             return self.tree[i]
         mid = (start + end) // 2
-        return self.search(start, mid, left, right, i * 2) + self.search(mid + 1, end, left, right, i * 2 + 1)
+        return self.combine_func(self.search(start, mid, left, right, i * 2), self.search(mid + 1, end, left, right, i * 2 + 1))
 
     def update_from_leaf(self, i, val):
         idx = self.location[i]
         self.tree[idx] = val
         while idx > 1:
             idx //= 2
-            self.tree[idx] = self.tree[idx * 2] + self.tree[idx * 2 + 1]
+            self.tree[idx] = self.combine_func(self.tree[idx * 2], self.tree[idx * 2 + 1])
